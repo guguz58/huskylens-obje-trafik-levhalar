743183b6006f494a0546c12fe9dc999b288bf10e
GÜNAY GÜZEL <102692490+guguz58@users.noreply.github.com>
2025-03-19 14:49:59 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,18 @@
+radio.setGroup(58)
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
+huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
 basic.forever(function () {
     huskylens.request()
-    basic.pause(100)
     if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
         huskylens.writeName(1, "Yaya")
         huskylens.writeOSD("Yaya Geciyor", 150, 30)
         radio.sendString("YAYA")
-    } else if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        huskylens.writeOSD("Dikkatli", 150, 30)
-        radio.sendString("Yaya harici nesne")
+        radio.sendNumber(3)
+        basic.pause(2000)
     } else {
         huskylens.writeOSD("Arac Gecebilir", 150, 30)
         radio.sendString("araba")
+        radio.sendNumber(4)
     }
 })
